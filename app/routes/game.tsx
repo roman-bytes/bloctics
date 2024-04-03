217f28e8c3de156406065758b24a9b86d28c64d6
Jacob Roman <jcbrmn06@gmail.com>
2024-04-03 09:39:47 -0400

    join game now goes to game

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import Bloctics from '~/components/Bloctics';
+import Bloctics from "~/components/Bloctics";
 import GameSetup from "~/components/GameSetup";
 import { ClientOnly } from "remix-utils";
 import type { LoaderArgs } from "@remix-run/node";
@@ -10,66 +10,82 @@ import { useLoaderData } from "@remix-run/react";
 import { blocticsMachineCookie } from "~/cookies";
 import { readCookie } from "~/routes/index";
 import { asyncInterpret } from "~/asynInterpret";
+import { log } from "console";
 
 export async function loader({ request }: LoaderArgs) {
   const stateConfig = await readCookie(request);
   if (!stateConfig) {
-    redirect('index');
+    redirect("index");
   }
 
   // Turn cookie into state
-  const currentState = await bloctics.resolveState(
-    State.create(stateConfig),
-  );
-  console.log('currentState1', currentState);
+  const currentState = await bloctics.resolveState(State.create(stateConfig));
+  console.log("currentState1", currentState);
 
-  return json({
-    state: currentState,
-  }, {
-    headers: {
-      "Set-Cookie": await blocticsMachineCookie.serialize(currentState),
+  return json(
+    {
+      state: currentState,
+    },
+    {
+      headers: {
+        "Set-Cookie": await blocticsMachineCookie.serialize(currentState),
+      },
     }
-  })
+  );
 }
 
 export const action = async ({ request, params: { state } }) => {
-  console.log('thuuuuuuuuuuuuuuuuss');
+  console.log("thuuuuuuuuuuuuuuuuss");
   const stateConfig = await readCookie(request);
   if (!stateConfig) return redirect(".."); // No cookie, so start over
 
   const currentState = bloctics.resolveState(stateConfig);
   const event = Object.fromEntries(await request.formData());
-  console.log('event------------------', event);
+  console.log("event------------------", event);
 
-  const nextState = await asyncInterpret(
-    bloctics,
-    3_000,
-    currentState,
-    event,
-  );
+  const nextState = await asyncInterpret(bloctics, 3_000, currentState, event);
+
+  console.log("NMEXT STATE", nextState);
 
   switch (event.type) {
-    case 'Exit':
-      return redirect('..');
-    case 'Create Room':
-      return json({
-        state: nextState,
-      }, {
-        headers: {
-          "Set-Cookie": await blocticsMachineCookie.serialize(nextState),
+    case "Exit":
+      return redirect("..");
+    case "Pass Init":
+      return json(
+        {
+          state: nextState,
+        },
+        {
+          headers: {
+            "Set-Cookie": await blocticsMachineCookie.serialize(nextState),
+          },
         }
-      });
-    case 'Join Room':
-      return json({
-        state: nextState,
-      }, {
-        headers: {
-          "Set-Cookie": await blocticsMachineCookie.serialize(nextState),
+      );
+    case "Create Room":
+      return json(
+        {
+          state: nextState,
+        },
+        {
+          headers: {
+            "Set-Cookie": await blocticsMachineCookie.serialize(nextState),
+          },
         }
-      });
+      );
+    case "Join Room":
+      return json(
+        {
+          state: nextState,
+        },
+        {
+          headers: {
+            "Set-Cookie": await blocticsMachineCookie.serialize(nextState),
+          },
+        }
+      );
     default:
       return json({
-        hello: 'world',
+        hello: "world",
       });
   }
 };
@@ -79,20 +95,20 @@ export default function Game() {
   const { context } = state;
 
   // Create util to deconstruct object
-  console.log('state', state);
+  console.log("state", state);
   const gameState = Object.keys(state.value);
   const gameChildState = Object.values(state.value);
-  console.log('gameState', gameState);
-  console.log('gameChildState', gameChildState);
+  console.log("gameState", gameState);
+  console.log("gameChildState", gameChildState);
 
   return (
     <ClientOnly>
       {() => (
         <>
-          {gameState.includes('Game Setup') && (
+          {gameState.includes("Game Setup") && (
             <GameSetup
-              showCreate={gameChildState.includes('Create Confirm')}
-              showJoin={gameChildState.includes('Enter Code')}
+              showCreate={gameChildState.includes("Create Confirm")}
+              showJoin={gameChildState.includes("Enter Code")}
               context={context}
             />
           )}
